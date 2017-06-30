594ac4acfae57f1f78d2445eef6b78e978e4325b
backtrackbasic <prasad.thakur1994@gmail.com>
2017-06-30 19:54:58 +0530

    single commit

---
+++ Includes/js/custom.js
@@ -0,0 +1,9 @@
+﻿//Right click Diasbale.......!
+
+$(document).on({
+    "contextmenu": function (e) {
+    alert('Right click Diable..')
+               // Stop the context menu
+        e.preventDefault();
+    },
+   });
